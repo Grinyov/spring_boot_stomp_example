73427550a1763f531430454a341cad156f921005
Vitaliy Grinyov <vgrinyov@gmail.com>
2017-04-04 22:23:11 +0300

    add a project structure

---
+++ src/main/java/com/grinyov/SpringBootStompExampleApplication.java
@@ -0,0 +1,12 @@
+package com.grinyov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStompExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStompExampleApplication.class, args);
+	}
+}
